005f4261f0c8b33cf77837153194dd0104144057
XanaxbaCK <hsener233@gmail.com>
2026-08-26 16:00:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -186,11 +186,11 @@ if "user_db" not in st.session_state:
         "elisa": "elisa",
         "alkan": "alkan",
         "büşra": "büşra",
-        "azad": "azad",
+        "azat": "azat",
     }
 
 if "premium_users" not in st.session_state:
-    st.session_state.premium_users = {"murat","sinemk","elisa","alkan","büşra","azad",}
+    st.session_state.premium_users = {"murat","sinemk","elisa","alkan","büşra","azat",}
 
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
